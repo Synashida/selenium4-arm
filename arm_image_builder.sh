2c0d6a727651112f9352fbbb8be68aee1c9574d7
Shinichi <ashida@nc-net.or.jp>
2021-09-02 10:52:10 +0900

    arm_image_builder.shを本リポジトリ用に修正 FirefoxはARM版でセッションのParseエラーが発生するので修正

--- arm_image_builder.sh
+++ arm_image_builder.sh
@@ -2,30 +2,30 @@
 
 # Base
 docker-compose -f docker-compose-arm-image-build.yml build --no-cache selenium-base
-docker tag docker-selenium_selenium-base:latest ncnetwork/selenium4-base-arm:latest
+docker tag selenium4-arm_selenium-base:latest ncnetwork/selenium4-base-arm:latest
 docker push ncnetwork/selenium4-base-arm:latest
 
 # HUB
 docker-compose -f docker-compose-arm-image-build.yml build --no-cache selenium-hub
-docker tag docker-selenium_selenium-hub:latest ncnetwork/selenium4-hub-arm:latest
+docker tag selenium4-arm_selenium-hub:latest ncnetwork/selenium4-hub-arm:latest
 docker push ncnetwork/selenium4-hub-arm:latest
 
 # Node Base
 docker-compose -f docker-compose-arm-image-build.yml build --no-cache selenium-node-base
-docker tag docker-selenium_selenium-node-base:latest ncnetwork/selenium4-node-base-arm:latest
+docker tag selenium4-arm_selenium-node-base:latest ncnetwork/selenium4-node-base-arm:latest
 docker push ncnetwork/selenium4-node-base-arm:latest
 
 # Node Chrome
 docker-compose -f docker-compose-arm-image-build.yml build --no-cache selenium-node-chrome
-docker tag docker-selenium_selenium-node-chrome:latest ncnetwork/selenium4-node-chrome-arm:latest
+docker tag selenium4-arm_selenium-node-chrome:latest ncnetwork/selenium4-node-chrome-arm:latest
 docker push ncnetwork/selenium4-node-chrome-arm:latest
 
 # Node Firefox
 docker-compose -f docker-compose-arm-image-build.yml build --no-cache selenium-node-firefox
-docker tag docker-selenium_selenium-node-firefox:latest ncnetwork/selenium4-node-firefox-arm:latest
+docker tag selenium4-arm_selenium-node-firefox:latest ncnetwork/selenium4-node-firefox-arm:latest
 docker push ncnetwork/selenium4-node-firefox-arm:latest
 
 # Node Edge
 #docker-compose -f docker-compose-arm-image-build.yml build --no-cache selenium-node-edge
-#docker tag docker-selenium_selenium-node-edge:latest ncnetwork/selenium4-node-edge-arm:latest
+#docker tag selenium4-arm_selenium-node-edge:latest ncnetwork/selenium4-node-edge-arm:latest
 #docker push ncnetwork/selenium4-node-edge-arm:latest
